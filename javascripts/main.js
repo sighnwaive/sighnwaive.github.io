1f0d03e4f6da9eda0f326dd56a452a2d6e6abb61
Dennis Bartlett <bartlett.dc.1@gmail.com>
2016-11-13 00:08:17 -0600

    Testing out mongo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+var markdown = new showdown.Converter(),
+
+var blogTemplate = Handlebars.compile($("#blogs-template").html());
+
+$(function(){
+  $.ajax({
+    url: 'http://ds151697.mlab.com:51697/dcbartlett_github_io/blogs',
+    type: 'get',
+    dataType: 'jsonp',
+    jsonp: 'jsonp', // mongod is expecting the parameter name to be called "jsonp"
+    success: function (data) {
+      console.log('success', data);
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      console.log('error', errorThrown);
+    }
+  });
+});
+
+$("#blogView").html(blogTemplate());
+
+Handlebars.registerHelper('blogs', function(blogEntries) {
+  var out = '<div id="blogs">';
+
+  for(var i=0, l=blogEntries.length; i<l; i++) {
+    out = out + '<div class="blog">' + markdown.makeHtml(blogEntries[i].body) + '</li>';
+  }
+
+  return out + '</div>';
+});
