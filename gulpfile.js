c192a5576e09f122c869e2dcd92f92c7c290306b
Dennis Bartlett <bartlett.dc.1@gmail.com>
2017-03-13 14:41:18 -0500

    Updates?

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,7 @@
 var gulp = require('gulp');
 var subtree = require('gulp-subtree');
-var clean = require('gulp-clean');
-var package = require('./package.json');
  
 gulp.task('deploy', function () {
     return gulp.src('./public/')
-        .pipe(subtree({
-		remote: 'origin',
-		branch: 'gh-pages',
-		message: 'Deploying v' + package.version
-	}))
+        .pipe(subtree())
 });
